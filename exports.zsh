a7cc273aa1b0314984ea7be94769d957310ffc5d
hashok <hars.ashok@gmail.com>
2019-01-12 16:04:10 -0800

    adding exports

---
+++ exports.zsh
@@ -0,0 +1,14 @@
+#Setup PATH 
+
+export PATH="/bin:/usr/bin:/usr/local/bin:"
+
+#Setup terminal, and turn on colors
+export TERM=xterm-256color
+export CLICOLOR=1
+export LSCOLORS=Gxfxcxdxbxegedabagacad
+
+#Enable color in grep
+export GREP_OPTIONS='--color=auto'
+export GREP_COLOR='3;33'
+
+
